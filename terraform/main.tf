ff2ab88addd862d93149729afb306b71828be589
Erick Shaffer <erick.shaffer321@gmail.com>
2025-03-18 12:38:27 -0600

    terraform bucket and keys

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,18 @@
-resource "b2_application_key" "example" {
-  key_name     = "test-b2-tfp-0000000000000000000"
+variable "b2_application_key" {
+  type        = string
+  description = "The name of the application"
+  default = "automeet"
+}
+
+resource "b2_application_key" "automeet" {
+  key_name     = var.b2_application_key
   capabilities = ["readFiles"]
 }
 
-data "b2_application_key" "example" {
-  key_name = b2_application_key.example.key_name
+data "b2_application_key" "automeet" {
+  key_name = b2_application_key.automeet.key_name
 }
 
 output "application_key" {
-  value = data.b2_application_key.example
+  value = data.b2_application_key.automeet.key_name
 }
