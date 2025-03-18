5e4b591418c27366f64fe5152d5f4dba3bfee379
Erick Shaffer <erick.shaffer321@gmail.com>
2025-03-18 13:35:32 -0600

    provision bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,12 @@ variable "b2_application_key" {
   default = "automeet"
 }
 
+variable "bucket_name" {
+  type = string
+  description = "Name of the b2 bucket to hold the audio data"
+  default = "automeet-bucket"
+}
+
 resource "b2_application_key" "automeet" {
   key_name     = var.b2_application_key
   capabilities = ["readFiles"]
@@ -15,4 +21,19 @@ data "b2_application_key" "automeet" {
 
 output "application_key" {
   value = data.b2_application_key.automeet.key_name
+}
+
+resource "b2_bucket" "example" {
+  bucket_name = var.bucket_name
+  bucket_type = "allPrivate"
+
+  bucket_info = {
+    "project" = "automeet"
+  }
+
+  default_server_side_encryption {
+    algorithm = "AES256"
+    mode      = "SSE-B2"
+  }
+
 }
