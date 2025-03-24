dd417f01e13807d45a70e4fc517345640975028b
Erick Shaffer <erick.shaffer321@gmail.com>
2025-03-24 16:17:23 -0600

    add redis password

--- src/shared/redis_client.py
+++ src/shared/redis_client.py
@@ -4,5 +4,6 @@ from config.config import settings
 redis_client = redis.Redis(
     host=settings.redis.host,
     port=settings.redis.port,
-    decode_responses=True
+    decode_responses=True,
+    password=settings.redis.password
 )
